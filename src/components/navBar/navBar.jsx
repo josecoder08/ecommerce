9637db88d775a1018621581bafea74ed62d980d0
jose vaudagna <vaudagnajose@gmail.com>
2022-02-18 18:29:56 -0300

    navBar

---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navBar.css'
+const navBar = () => {
+  return (
+    <ul>
+        <li>muebles</li>
+        <li>camas</li>
+        <li>sillones</li>
+         <li>mesa</li> 
+    </ul>
+  )
+}
+
+export default navBar
